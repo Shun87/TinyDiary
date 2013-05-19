cc970947ceebda74e3d9b15577a49b33cfbf22d7
chenshun <chenshun87@163.com>
2013-05-20 00:29:31 +0800

    修改日历图标

--- iDiary/kal/KalViewController.m
+++ iDiary/kal/KalViewController.m
@@ -47,6 +47,7 @@ NSString *const KalDataSourceChangedNotification = @"KalDataSourceChangedNotific
     self.initialDate = date;
     self.selectedDate = date;
     self.title = NSLocalizedString(@"Calendar", nil);
+    self.tabBarItem.image = [UIImage imageNamed:@"calendar"];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(significantTimeChangeOccurred) name:UIApplicationSignificantTimeChangeNotification object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(reloadData) name:KalDataSourceChangedNotification object:nil];
   }
